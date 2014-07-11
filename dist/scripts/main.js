e397aa2d78701b4c37826d402bc6cc042e549772
Taylor Smith <tsmit182@gmail.com>
2014-07-11 12:40:53 -0400

    gulp build

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,1 +1,11 @@
 console.log('The Iron Yard Rocks');
+var SomethingConstructed = Backbone.Model.extend ({
+		name: '',
+		type: 'thing',
+		isGood: true,
+	});
+
+
+var goodThingConstructed = new SomethingConstructed ({
+     name: 'A Good Thing',
+ });
