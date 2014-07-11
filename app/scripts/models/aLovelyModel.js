9a98e52d034c58f3be024caccc7c08a887326f23
Taylor Smith <tsmit182@gmail.com>
2014-07-11 12:23:33 -0400

    add a model

---
+++ app/scripts/models/aLovelyModel.js
@@ -0,0 +1,10 @@
+var SomethingConstructed = Backbone.Model.extend ({
+		name: '',
+		type: 'thing',
+		isGood: true,
+	});
+
+
+var goodThingConstructed = new SomethingConstructed ({
+     name: 'A Good Thing',
+ });
